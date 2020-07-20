b734cdadb187b825c711b32bd570c394dd5718c9
tangobeer <ritwikgoel@yahoo.com>
2020-07-20 19:27:25 +0530

    fix:multimail changes

--- Mail.py
+++ Mail.py
@@ -12,21 +12,21 @@ PATH='/home/tangobeer/Documents/chromedriver'
 
 
 
-# print("Enter the message to be bulk sent")
-# MESSAGE = input()
-# print(MESSAGE)
-# # creating an empty list 
-# EMAILS = [] 
+print("Enter the message to be bulk sent")
+MESSAGE = input()
+print(MESSAGE)
+# creating an empty list 
+EMAILS = [] 
   
-# # number of elemetns as input 
-# NUM_OF_MAILS=int(input("Enter number of mails to be sent: "))
+# number of elemetns as input 
+NUM_OF_MAILS=int(input("Enter number of mails to be sent: "))
   
-# # iterating till the range 
-# for i in range(0, NUM_OF_MAILS): 
-#     ele = input("Please enter the mail address") 
-#     EMAILS.append(ele) # adding the element 
+# iterating till the range 
+for i in range(0, NUM_OF_MAILS): 
+    ele = input("Please enter the mail address") 
+    EMAILS.append(ele) # adding the element 
 
-# print(EMAILS)
+print(EMAILS)
 
 # write a gmail opening script
 driver=webdriver.Chrome(PATH)
@@ -41,17 +41,23 @@ pass_in_mail.send_keys(Keys.RETURN)
 
 driver.implicitly_wait(3)
 time.sleep(5)
-COMPOSE=driver.find_element_by_xpath("//*[contains(text(), 'Compose')]")
-COMPOSE.click()
-driver.implicitly_wait(3)
-mail_to=driver.find_element_by_name("autocomplete")
-mail_to.send_keys("ritwikgoel@yahoo.com")
-mail_subject=driver.find_element_by_xpath("//input[@title-translate-context='info']")
-mail_subject.send_keys("ANother twst")
-iframe = driver.find_element_by_tag_name('iframe')
-driver.switch_to_frame(iframe)
-mail_content=driver.find_element_by_xpath("//div")
-mail_content.send_keys("LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM ")
-driver.switch_to.default_content()
-SEND=driver.find_element_by_xpath("//*[contains(text(), 'Send')]")
-SEND.click()
+
+for i in range (0, NUM_OF_MAILS):
+    COMPOSE=driver.find_element_by_xpath("//*[contains(text(), 'Compose')]")
+    COMPOSE.click()
+    driver.implicitly_wait(3)
+    mail_to=driver.find_element_by_name("autocomplete")
+    mail_to.send_keys(EMAILS[i])
+    mail_subject=driver.find_element_by_xpath("//input[@title-translate-context='info']")
+    mail_subject.send_keys("ANother twst")
+    iframe = driver.find_element_by_tag_name('iframe')
+    driver.switch_to.frame(iframe)
+    mail_content=driver.find_element_by_xpath("//div")
+    mail_content.send_keys(MESSAGE)
+    driver.switch_to.default_content()
+    SEND=driver.find_element_by_xpath("//*[contains(text(), 'Send')]")
+    SEND.click()
+    time.sleep(5)
+
+driver.implicitly_wait(2)
+driver.quit()
