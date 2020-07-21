534472b7dc094a4789c54a9abae48fd9e87dd1f8
Ritwik Goel <55455435+ritwikgoel@users.noreply.github.com>
2020-07-21 09:54:04 +0530

    Update Mail.py

--- Mail.py
+++ Mail.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-import pyautogui as pg
+#import pyautogui as pg
 from selenium.webdriver.support import expected_conditions as EC
 import sys
 
